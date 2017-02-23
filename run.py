4afa233092b3dcd2647207928ec0bef6c668e4ff
Tobias Guggenmoser <tobias.guggenmoser@esa.int>
2017-02-23 16:50:30 +0100

    Added directory structure and run.py script.

---
+++ run.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import tempfile
+import zipfile
+import glob
+
+
+def main(args):
+    print("Nothing implemented. Yet!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("run_id", type=int)
+    args = parser.parse_args()
+
+    outpath = os.path.join(".", "outputs", "%03d" % args.run_id)
+    print("Preparing output directory %s" % outpath)
+    if os.path.exists(outpath):
+        bak = tempfile.mktemp(prefix="bak-%03d_" % args.run_id, dir=os.path.join(".", "outputs"))
+        os.rename(outpath, bak)
+    os.mkdir(outpath)
+
+    sources = glob.glob("*.py")
+    zip_dst = os.path.join(outpath, "sources.zip")
+
+    print("Writing %s to %s" % (sources, zip_dst))
+    with zipfile.ZipFile(zip_dst, 'w') as zip:
+        for filename in sources:
+            zip.write(filename)
+    print("Setup done, launching main function.")
+
+    main(args)
